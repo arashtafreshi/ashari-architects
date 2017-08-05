1c148315d99ba4380827bc43828854103b0bb2c9
Arash <arash.nasirtafreshi@gmail.com>
2017-08-05 15:28:12 -0400

    Deploy script added

---
+++ DeployScripts.sh
@@ -0,0 +1,8 @@
+dotnet restore
+dotnet publish
+
+sudo rm -r /var/dotnetafshin/*
+sudo cp -a /home/arash/afshin/ashari-architects.com/bin/Debug/netcoreapp1.1/publish/* /var/dotnetafshin/
+
+sudo service supervisor stop
+sudo service supervisor start
